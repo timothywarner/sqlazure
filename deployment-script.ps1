c21fb40503ab3371315bf15b806a33fb4f0007af
Tim Warner <timothywarner316@gmail.com>
2021-08-16 08:24:25 -0500

    finish ppt for aug 2021 delivery

--- deployment-script.ps1
+++ deployment-script.ps1
@@ -1,22 +1,16 @@
 $subscriptionID = ''
 $tenantID = ''
-$deploymentName = ''
-$resourceGroupName = ''
-$templatePath = ''
+$deploymentName = 'class-db'
+$resourceGroupName = 'oreilly'
+$templatePath = './azure-sql-aworks.bicep'
 $context = Get-AzContext
 
 if (($context.Subscription -ne $subscriptionID) -and ($context.Tenant -ne $tenantID)) {
-  Clear-Host
-  Write-Output 'You need to log in'
+  Connect-AzAccount -Tenant $tenantID
 }
-  Clear-Host
-  Write-Output 'You good.'
-
-
-
 
 New-AzResourceGroupDeployment -Name $deploymentName `
   -ResourceGroupName $resourceGroupName `
-  -TemplateFile $templatePath
+  -TemplateFile $templatePath `
   -Mode Incremental `
   -Verbose
